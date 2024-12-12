4952e6e37737390f005c5362fadf6388549e64dd
jakeyks2 <jakob.khan-springer@mail.bcu.ac.uk>
2024-12-12 11:33:46 +0000

    Fully implemented emf reader and added placeholder win/loss setup

--- Assets/Scripts/GhostSpawner.cs
+++ Assets/Scripts/GhostSpawner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class GhostSpawner : MonoBehaviour
 {
@@ -10,7 +11,12 @@ public class GhostSpawner : MonoBehaviour
     public Camera xrCamera;
     public GameObject ghost;
 
+    [SerializeField]
+    string sceneToLoad;
+
     float ghostTimer = 0.0f;
+    float enrageTimer = 0.0f;
+    int ghostHealth = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -22,16 +28,37 @@ public class GhostSpawner : MonoBehaviour
     void Update()
     {
         ghostTimer += Time.deltaTime;
+        enrageTimer += Time.deltaTime;
+        if (enrageTimer >= 300.0f)
+        {
+            Debug.Log("loss");
+            SceneManager.LoadScene(sceneToLoad);
+        }
         if (ghostTimer >= 15.0f)
         {
-            ghostTimer = 0.0f;
-            if (ghost == null)
-            {
-                ghost = Instantiate(ghostPrefab);
-                ghost.GetComponent<Billboard>().playerCamera = xrCamera;
-            }
-            ghost.transform.position = pointGenerator.GetRandomPointInRoom(xrCamera.transform.position, 3.0f);
-            ghost.GetComponent<Renderer>().enabled = false;
+            MoveGhost();
+        }
+    }
+
+    public void MoveGhost()
+    {
+        ghostTimer = 0.0f;
+        if (ghost == null)
+        {
+            ghost = Instantiate(ghostPrefab);
+            ghost.GetComponent<Billboard>().playerCamera = xrCamera;
+        }
+        ghost.transform.position = pointGenerator.GetRandomPointInRoom(xrCamera.transform.position, 3.0f);
+        ghost.GetComponent<Renderer>().enabled = false;
+    }
+
+    public void DamageGhost(int amount)
+    {
+        ghostHealth -= amount;
+        if (ghostHealth <= 0)
+        {
+            Debug.Log("win");
+            SceneManager.LoadScene(sceneToLoad);
         }
     }
 }
--- Assets/Scripts/InGameUI.cs
+++ Assets/Scripts/InGameUI.cs
@@ -14,13 +14,15 @@ public class InGameUI : MonoBehaviour
     Label temperatureLabel;
     Button torchButton;
     VisualElement flash;
-    
+    VisualElement emfContainer;
+    List<VisualElement> emfLevels = new();
 
     bool uiLoaded = false;
 
     float flashTimer = 0.0f;
-    float maxFlashTimer = 0.05f;
+    float maxFlashTimer = 0.2f;
     bool isFlashActive = false;
+    bool didHitGhost = false;
 
     // Start is called before the first frame update
     void Start()
@@ -35,6 +37,18 @@ public class InGameUI : MonoBehaviour
         root.RegisterCallback<GeometryChangedEvent>(OnGeometryChanged);
 
         torchButton.clicked += Torch;
+
+        emfContainer = root.Q("emf-container");
+
+        for (int i = 0; i < 5; i++)
+        {
+            VisualElement emfLevel = new VisualElement();
+            emfLevel.style.backgroundColor = Color.HSVToRGB((float)i / 12.0f, 1.0f, 1.0f);
+            emfLevel.style.position = Position.Relative;
+            emfLevel.style.flexGrow = 1.0f;
+            emfContainer.Add(emfLevel);
+            emfLevels.Add(emfLevel);
+        }
     }
 
     // Update is called once per frame
@@ -43,10 +57,17 @@ public class InGameUI : MonoBehaviour
         if (uiLoaded)
         {
             progressBar.style.height = progressBarContainer.resolvedStyle.height * (sensors.temperature + 20.0f) / 100.0f;
-            progressBar.style.top = progressBarContainer.resolvedStyle.height * (100.0f - sensors.temperature) / 100.0f;
+            progressBar.style.top = progressBarContainer.resolvedStyle.height * (80.0f - sensors.temperature) / 100.0f;
 
             float temperatureRatio = sensors.temperature / sensors.baseTemperature;
-            progressBar.style.backgroundColor = new(Color.HSVToRGB(2.0f / 3.0f - temperatureRatio * 2.0f / 3.0f, 1.0f, 1.0f));
+            progressBar.style.backgroundColor = Color.HSVToRGB(2.0f / 3.0f - temperatureRatio * 2.0f / 3.0f, 1.0f, 1.0f);
+
+            temperatureLabel.text = sensors.temperature.ToString("F0") + "°C";
+
+            for (int i = 0; i < emfLevels.Count; i++)
+            {
+                emfLevels[i].style.backgroundColor = sensors.emf >= i * 25.0f ? Color.HSVToRGB((float)i / 12.0f, 1.0f, 1.0f) : Color.clear;
+            }
         }
 
         if (isFlashActive)
@@ -55,8 +76,15 @@ public class InGameUI : MonoBehaviour
             if (flashTimer >= maxFlashTimer)
             {
                 flashTimer = 0.0f;
-                flash.style.backgroundColor = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+                flash.style.backgroundColor = Color.clear;
                 isFlashActive = false;
+                if (didHitGhost)
+                {
+                    didHitGhost = false;
+                    sensors.ghostSpawner.ghost.GetComponent<Renderer>().enabled = false;
+                    sensors.ghostSpawner.DamageGhost(1);
+                    sensors.ghostSpawner.MoveGhost();
+                }
             }
         }
     }
@@ -69,14 +97,23 @@ public class InGameUI : MonoBehaviour
         progressBarContainer.style.top = width * 0.05f;
         progressBarContainer.style.left = width * 0.05f;
         torchButton.style.height = width * 0.3f;
-        Debug.Log(width * 0.05f);
-        Debug.Log(progressBarContainer.resolvedStyle.height);
-        Debug.Log(temperatureLabel.parent);
+        temperatureLabel.style.left = width * 0.1f + progressBarContainer.resolvedStyle.width;
+        temperatureLabel.style.top = width * 0.05f;
+        temperatureLabel.style.fontSize = progressBarContainer.resolvedStyle.width;
+        emfContainer.style.width = width * 0.5f;
+        emfContainer.style.height = width * 0.1f;
+        emfContainer.style.top = progressBarContainer.resolvedStyle.height / 2.0f;
+        emfContainer.style.right = width * 0.05f;
     }
 
     void Torch()
     {
         flash.style.backgroundColor = new Color(1.0f, 1.0f, 0.8f, 0.2f);
         isFlashActive = true;
+        if (sensors.ghostDistance <= 1.0f && sensors.emf >= 100.0f && sensors.ghostSpawner.ghost != null)
+        {
+            didHitGhost = true;
+            sensors.ghostSpawner.ghost.GetComponent<Renderer>().enabled = true;
+        }
     }
 }
